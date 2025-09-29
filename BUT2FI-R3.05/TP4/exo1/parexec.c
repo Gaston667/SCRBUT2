b59c4b83dd1f51217296f4aa7c71a6046c4def76
Diallo-VM-fbleau <algassimoupelleldiallo2005@gmail.com>
2025-09-29 16:07:20 +0200

    tp4

---
+++ BUT2FI-R3.05/TP4/exo1/parexec.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <assert.h>
+#include <unistd.h>
+#include <stdlib.h>
+int main(int argc, char* argv[]){
+    if (argc < 4) {
+        printf("Usage: %s <./proc> <N> <nombre>\n", argv[0]);
+        return 1;
+    }
+    // initialisation
+    int const N = atoi(argv[2]);
+    pid_t p= getpid();
+    printf("JE SUIS LE PERE %d.\n", p);
+    int nbr_proc = argc - 2;
+    int nbr_actif = 0;
+   
+
+  
+
+    for(int i=0; i < nbr_proc; i++){
+        while (nbr_actif == N)
+        {
+            // pour la recuperation les fils en cour d'ecution
+            for (int  i = 0; i < nbr_actif; i++){
+                wait(NULL);        
+            }
+            printf("...\n");
+            nbr_actif--;
+        }
+        p = fork();
+        if (p == 0){
+            execl(argv[1], argv[1], argv[i+2], NULL);
+            assert(0);
+        }
+        nbr_actif ++;
+    }   
+
+    // pour la recuperation les fils restant
+    for (int  i = 0; i < nbr_proc; i++){
+        wait(NULL);        
+    }
+
+    return 0;
+}
