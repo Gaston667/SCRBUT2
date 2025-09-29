401ac6d26b52b3ec5b1247356feaa0eecc14b26e
Diallo-VM-fbleau <algassimoupelleldiallo2005@gmail.com>
2025-09-29 16:15:57 +0200

    commit

--- BUT2FI-R3.05/TP4/exo1/parexec.c
+++ BUT2FI-R3.05/TP4/exo1/parexec.c
@@ -13,7 +13,7 @@ int main(int argc, char* argv[]){
     int const N = atoi(argv[2]);
     pid_t p= getpid();
     printf("JE SUIS LE PERE %d.\n", p);
-    int nbr_proc = argc - 2;
+    int nbr_proc = argc - 3;
     int nbr_actif = 0;
    
 
@@ -23,15 +23,14 @@ int main(int argc, char* argv[]){
         while (nbr_actif == N)
         {
             // pour la recuperation les fils en cour d'ecution
-            for (int  i = 0; i < nbr_actif; i++){
+            for (int  i = 0; i <= nbr_actif; i++){
                 wait(NULL);        
             }
-            printf("...\n");
             nbr_actif--;
         }
         p = fork();
         if (p == 0){
-            execl(argv[1], argv[1], argv[i+2], NULL);
+            execl(argv[1], argv[1], argv[i+3], NULL);
             assert(0);
         }
         nbr_actif ++;
